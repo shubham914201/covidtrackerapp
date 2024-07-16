fba9fdaf073a8c2c0962f4eab0ffad3394458b68
Shubham Kumar <svkshubham9142@gmail.com>
2024-07-17 04:26:56 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import Covid from "./components/covid"
-
+import Covid1 from "./components/covid1"
 function App() {
-   return <Covid/>;
+   return <div>
+   <Covid/>;
+   <Covid1/>;
+   </div>
 }
 
 export default App
---
+++ src/components/covid1.jsx
@@ -0,0 +1,4 @@
+function Covid1(){
+   return <h1>Hello</h1>;
+}
+export default Covid1;
